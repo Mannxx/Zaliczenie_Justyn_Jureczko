68a1ddbe1065e560676884c25d8236ab0c950560
Mannxx <62535599+Mannxx@users.noreply.github.com>
2020-09-13 22:36:17 +0200

    JustynJureczko_ratunek

---
+++ JustynJureczko_LastChance/Assets/Scripts/PointCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+    public Text pointText;
+    private int points = 0;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = "Score:" + points.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+
+    {
+        if (other.gameObject.tag == "Box")
+
+    {
+            points += 1;
+            Destroy(other.gameObject);
+        }
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
